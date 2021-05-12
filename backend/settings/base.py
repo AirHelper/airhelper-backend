9a6d8658409d2f83dfc34d3c80159e078b934dae
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-12 22:18:17 +0900

    sqlite 이용해서 테스트방식으로 변경테스트

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'cert.CustomUser'
+
+
--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -16,14 +16,21 @@ import json
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'airhelperdev',
-        'USER': 'airhelper',
-        'PASSWORD': 'airhelper',
-        'HOST': 'psqldb',
-        'PORT': 5432,
+if sys.argv[1] == 'test':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'airhelperdev',
+            'USER': 'airhelper',
+            'PASSWORD': 'airhelper',
+            'HOST': 'psqldb',
+            'PORT': 5432,
+        }
+    }
