f083418580de5eb2de0e036e1a9ac383ab50358b
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-12 22:29:14 +0900

    DB 정보 변경

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -16,21 +16,13 @@ import json
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if sys.argv[0] == 'pytest':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airhelperdev',
+        'USER': 'airhelper',
+        'PASSWORD': 'airhelper',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'airhelperdev',
-            'USER': 'airhelper',
-            'PASSWORD': 'airhelper',
-            'HOST': 'psqldb',
-            'PORT': 5432,
-        }
-    }
+}
