9cd6a833e1b583ab24cf4c25e7423e17da102ebe
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-17 23:42:34 +0900

    gunicorn + nginx

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -122,5 +123,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'cert.CustomUser'
-
 
