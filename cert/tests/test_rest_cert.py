409735e320149533ad6370f878bd838e4632de55
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-20 00:03:05 +0900

    테스트코드 작성중

--- cert/tests/test_rest_cert.py
+++ cert/tests/test_rest_cert.py
@@ -4,7 +4,18 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from cert.models import CustomUser
 from cert.serializers import CustomUserSerializer
-#
-# class UserTests(APITestCase):
-#     def setUp(self):
 
+
+class UserTests(APITestCase):
+    def test_create_user(self):
+        url = reverse('user_CR')
+        data = {
+            'name': 'tester',
+            'call_sign': 'JEFF',
+            'email': 'tester@test.com',
+            'password': '1234',
+            'user_id': 'test'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
