a78e756550a6f953d0ee93263e14edefa2e368b2
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-20 20:37:55 +0900

    테스트 시각화 적용 및 커버리지 향상

--- cert/tests/test_model.py
+++ cert/tests/test_model.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from cert.models import CustomUser, UserManager
-from cert.serializers import CustomUserSerializer
+from cert.serializers import CustomUserSerializer, CreateUserSerializer
 import os
 
 
@@ -8,14 +8,16 @@ class CustomUserModelTest(TestCase):
     def setUp(self):
         self.user = CustomUser.objects.create(
             password='test',
-            user_id="testuser",
-            email='dev@dev.com',
-            name='Jun',
-            call_sign='JEFF'
+            user_id="testuser321321",
+            email='dev321312@dev.com',
+            name='Ju321n',
+            call_sign='JEFF3213213'
         )
         self.user.set_password('1234')
         self.user.save()
         self.serialized_user = CustomUserSerializer(self.user).data
+        self.assertEqual(self.user.has_perm('cert'), True)
+        self.assertEqual(self.user.has_module_perms('cert'), True)
 
     def test_create_models(self):  # 생성테스트
         self.user1 = CustomUser.objects.create(
@@ -33,7 +35,25 @@ class CustomUserModelTest(TestCase):
             user_id='test',
             email="cartoonpoet@naver.com",
             password='dfsdfsd',
-            name='jun',
+            name='jun3213',
             call_sign='MAD'
         )
+        self.assertEqual(CustomUser.objects.count(), 3)
 
+        self.staff_user = CustomUser.objects.create_staffuser(
+            user_id='staff',
+            email='staff@naver.com',
+            name='staff_person',
+            call_sign='STAFF',
+            password='1232'
+        )
+        self.assertEqual(CustomUser.objects.count(), 4)
+
+        self.super_user = CustomUser.objects.create_superuser(
+            user_id='root',
+            email='root@root.com',
+            name='root',
+            call_sign='ROOT',
+            password='root'
+        )
+        self.assertEqual(CustomUser.objects.count(), 5)
