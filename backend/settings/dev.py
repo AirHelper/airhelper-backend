667a97acbcbd22cb8460712b312fa70f1b6ece92
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-07-28 00:23:50 +0900

    redis 설정중

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -21,7 +21,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('172.23.0.1', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         }
     },
 }
