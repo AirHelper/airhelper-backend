df91b543dccc35b4be39d67b80499957b6b727b5
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-08-03 00:06:49 +0900

    방장이 연결 끊으면 방삭제 조치완료

--- room/consumers.py
+++ room/consumers.py
@@ -3,25 +3,33 @@ from channels.generic.websocket import WebsocketConsumer
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from .models import Room
 
 
 class CreateRoom(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-
+        self.room_group_name = 'room'
         # Join room group
         await self.channel_layer.group_add(
+            self.room_group_name,
             self.channel_name
         )
 
         await self.accept()
 
     async def disconnect(self, close_code):
+        await self.delete_room()
+
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    @database_sync_to_async
+    def delete_room(self):
+        Room.objects.filter(title=self.room_name).delete()
 
 
 class AttendRoom(AsyncWebsocketConsumer):
