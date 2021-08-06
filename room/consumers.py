c5859372019dbdc6ef44301f3ffd884ef15fb234
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-08-06 16:41:18 +0900

    URL 주소 room_id로 변경

--- room/consumers.py
+++ room/consumers.py
@@ -4,14 +4,19 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import Room
+from urllib import parse
 
 
 class CreateRoom(AsyncWebsocketConsumer):
     http_user = True
 
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_name = parse.unquote(self.scope['url_route']['kwargs']['room_name'])
         self.room_group_name = 'game_%s' % self.room_name
+
+        print(self.room_name)
+        print(self.room_group_name)
+
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -38,7 +43,7 @@ class CreateRoom(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def delete_room(self):
-        Room.objects.filter(title=self.room_name).delete()
+        Room.objects.filter(id=self.room_name).delete()
 
 
 class AttendRoom(AsyncWebsocketConsumer):
