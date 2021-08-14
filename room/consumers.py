e8da6ed5306908d4c9b53fc2e7a76b4914072816
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-08-14 18:01:42 +0900

    유저 방 입장, 퇴장 완료

--- room/consumers.py
+++ room/consumers.py
@@ -3,13 +3,11 @@ from channels.generic.websocket import WebsocketConsumer
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from asgiref.sync import sync_to_async
 from .models import Room, AttendedUser
 from .serializers import AttendedUserSerializer, AttendUserSerializer
 from urllib import parse
-from django.core import serializers
-from .views import AttendedUserViewSet
 import json
+
 
 class CreateRoom(AsyncWebsocketConsumer):
     http_user = True
@@ -17,10 +15,6 @@ class CreateRoom(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = parse.unquote(self.scope['url_route']['kwargs']['room_name'])
         self.room_group_name = 'game_%s' % self.room_name
-        print(self.room_name)
-        print(self.room_group_name)
-        print('채녈명 : '+self.channel_name)
-
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -35,8 +29,8 @@ class CreateRoom(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'all_disconnect',
-                'message': 'disconnect'
+                'type': 'user_disconnect',
+                'user': self.my_user_id
             }
         )
         # Leave room group
@@ -45,14 +39,44 @@ class CreateRoom(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+    async def user_disconnect(self, event):
+        data = await self.get_attenduser()
+        await self.send(text_data=data)
+
     @database_sync_to_async
     def delete_room(self):
         Room.objects.filter(id=self.room_name).delete()
 
+# Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+
+        if text_data_json['type'] == 'user_attend':
+            self.my_user_id = text_data_json['user']
+            await self.save_attenduser(text_data_json['user'], text_data_json['team'])
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            text_data_json
+        )
+
+    @database_sync_to_async
+    def save_attenduser(self, user_id, team):
+        return AttendedUser.objects.create(
+            user_id=user_id,
+            team=team,
+            room_id=self.room_name
+        )
+
     async def user_attend(self, event):
-        attended_users = AttendedUser.objects.filter(room=self.room_name).all()
-        serializer = AttendedUserSerializer(attended_users, many=True)
-        await self.send(text_data=serializer.data)
+        data = await self.get_attenduser()
+        await self.send(text_data=data)
+
+    @database_sync_to_async
+    def get_attenduser(self):
+        attend_users = AttendedUser.objects.all()
+        serializer = AttendedUserSerializer(attend_users, many=True)
+        return json.dumps(serializer.data)
 
 
 class AttendRoom(AsyncWebsocketConsumer):
@@ -62,6 +86,9 @@ class AttendRoom(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'game_%s' % self.room_name
 
+        if await self.room_exists() is not True:
+            self.my_user_id = 0
+            return
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -70,16 +97,40 @@ class AttendRoom(AsyncWebsocketConsumer):
 
         await self.accept()
 
+    @database_sync_to_async
+    def room_exists(self):
+        return Room.objects.filter(id=self.room_name).exists()
+
     async def disconnect(self, close_code):
+        await self.delete_attend_user()
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'user_disconnect',
+                'user': self.my_user_id
+            }
+        )
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
+    async def user_disconnect(self, event):
+        data = await self.get_attenduser()
+        await self.send(text_data=data)
+
+    @database_sync_to_async
+    def delete_attend_user(self):
+        AttendedUser.objects.filter(user_id=self.my_user_id).delete()
+
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+
+        if text_data_json['type'] == 'user_attend':  # 유저 방 입장
+            self.my_user_id = text_data_json['user']
+            await self.save_attenduser(text_data_json['user'], text_data_json['team'])
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -87,8 +138,7 @@ class AttendRoom(AsyncWebsocketConsumer):
         )
 
     async def user_attend(self, event):
-        attend_user = await self.save_attenduser(event['user'], event['team'])
-        data = await self.create_attenduser()
+        data = await self.get_attenduser()
         await self.send(text_data=data)
 
     @database_sync_to_async
@@ -100,7 +150,7 @@ class AttendRoom(AsyncWebsocketConsumer):
         )
 
     @database_sync_to_async
-    def create_attenduser(self):
+    def get_attenduser(self):
         attend_users = AttendedUser.objects.all()
         serializer = AttendedUserSerializer(attend_users, many=True)
         return json.dumps(serializer.data)
