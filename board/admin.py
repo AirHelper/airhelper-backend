b0ef2304e8c11faa980e32c4a879729265dd9c24
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-08-21 16:28:31 +0900

    admin site register

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Board, BoardMedia, BoardComment
 
 # Register your models here.
+admin.site.register(Board)
+admin.site.register(BoardMedia)
+admin.site.register(BoardComment)
